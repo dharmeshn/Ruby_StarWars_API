ddb4e15f8726902110084309e747a487ee6a62e4
Dharmesh N <dharmesh.natuarlal@hotmail.com>
2023-05-09 20:01:41 +0100

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,51 @@
+# Ruby API task
+
+>Using the ruby language and the API at https://swapi.dev/ you will form and output a sentence following this format:
+>
+>`<honourific> <surname> and <honourific> <surname> cruising around in their <starship>`
+>
+>For example:
+>`Ms. Lars and Mr. Skywalker cruising around in their X-wing`
+
+>Where:
+>-	Two characters are randomly selected from the API by the script up to a maximum ID of 15
+>-	Each character will be referred to as their honourific (Mr. or Ms. for male or female) according to gender then surname
+>-	A starship will be randomly selected from the API by the script up to a maximum ID of 30
+
+
+# Ruby Star Wars API Sentence Generator
+
+I have created Ruby script using HTTParty and RSpec dependencies that generates a sentence in the required output format using the Star Wars API:
+
+### Prerequisites
+
+To run this script, you will need to have the following installed:
+
+- Ruby (You can download it from [https://www.ruby-lang.org/en/downloads/](https://www.ruby-lang.org/en/downloads/))
+- HTTParty gem
+- RSpec gem
+
+### Setup
+
+1. Clone this repository or download the files.
+2. Open a terminal and navigate to the directory containing the files.
+3. Run the following command to install the required `HTTParty` and `RSpec` gems :
+
+```
+gem install httparty
+gem install rspec
+```
+OR
+```
+bundle install
+```
+
+
+
+## Test case
+
+This script includes a RSpec test to ensure that the generated sentence matches the required format. To run the test to generate sentence, simply run the following command:
+
+```
+rspec star_Wars_API.spec.rb
+```
---
+++ star_Wars_API.spec.rb
@@ -0,0 +1,70 @@
+require 'httparty'
+
+describe 'Star Wars API' do
+  it 'should return a sentence with required output format' do
+
+    BASE_URL = 'https://swapi.dev/api/'   # Base URL of the API
+
+    # Method to get a random character from the API
+    def get_random_character
+
+      # Get a random character
+      random_id = rand(1..15)
+      response = HTTParty.get("#{BASE_URL}people/#{random_id}")
+      character = response['name']
+      gender = response['gender']
+
+
+      # Determine the honourific based on gender
+      if gender == 'male'
+        honourific = 'Mr.'
+      elsif gender == 'female'
+        honourific = 'Ms.'
+      else
+        honourific = ''
+      end
+
+      # Extract the surname from the character name
+      surname = character.split.last
+
+      # Return the honourific and surname
+      return [honourific, surname]
+    end
+
+    # Method to get a random starship from the API
+    def get_random_starship
+      # Get a random starship
+      random_id = rand(1..30)
+      response = HTTParty.get("#{BASE_URL}starships/#{random_id}")
+      starship = response['name']
+
+      # Retry if the starship name is blank
+      if starship.nil? || starship.empty?
+        return get_random_starship
+      end
+
+      # Return the starship
+      return starship
+    end
+
+    # Generate the sentence
+    def generate_sentence
+      # Get two random characters
+      character1 = get_random_character
+      character2 = get_random_character
+
+      # Get a random starship
+      starship = get_random_starship
+
+      # Output the sentence
+      sentence = "#{character1[0]} #{character1[1]} and #{character2[0]} #{character2[1]} cruising around in their #{starship}"
+      puts sentence
+
+     expect(sentence).to match(/^\s*(Mr\.|Ms\.|)? [A-Za-z\w\-]+ and (Mr\.|Ms\.|)? [A-Za-z\w\-]+ cruising around in their [\w\-\s]+$/)
+
+    end
+    # Call the method to generate the sentence
+    generate_sentence
+    puts '***** NOTE : Where gender is not specified Male or Female, kept that honourific blank, while generating sentence *****'
+  end
+end
